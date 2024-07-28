4c7a5fc07260e0e42ca3c2bea83432ac84e5cf20
Pavan <pavchadrive@gmail.com>
2024-07-27 20:25:00 -0400

    Created fruit collector game

---
+++ game_practice/pygame_base_template.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.locals import *
+
+# start pygame
+pygame.init()
+
+# get screen with width and height
+W, H = 1000, 800
+screen = pygame.display.set_mode((W, H))
+
+# get clock for frame setting
+clock = pygame.time.Clock()
+
+# main game loop
+running = True
+while running:
+    # if player clicks X then exit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    ###############################
+    # Game logic updates
+    ###############################
+    # ...
+
+    ###############################
+    # Screen updates
+    ###############################
+    # clear screen
+    screen.fill('black')
+
+    # graphics changes
+    # ....
+
+    # show renderd graphics
+    pygame.display.flip()
+
+    # limit fps to 60
+    clock.tick(60)
+
+pygame.quit()
